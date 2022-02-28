a2db55be76fc7cacf6990b236637c667c883db0b
Diana Marcelin <marcelindiana@yahoo.com>
2022-02-28 11:38:12 -0500

    created req

--- src/users.js
+++ src/users.js
@@ -3,6 +3,7 @@ exports.createUser = (req, res) => {
   if (!req.body || !req.body.email || req.body.password) {
     //invalid request
     res.status(400).send("Invalid Request");
+    return;
   }
   const newUser = {
     email: req.body.email,
@@ -10,12 +11,12 @@ exports.createUser = (req, res) => {
     isAdmin: false,
     userRole: 5,
   };
-  const db = connectDb();
-  db.collection("users")
-    .add(newUser) //forcing shape of new user >> .add(newUser)
-    .then((doc) => {
-      //TODO:create a jwt & send back token
-      res.send("Account created!");
-    })
-    .catch((err) => res.status(500).send(err));
+  //   const db = connectDb();
+  //   db.collection("users")
+  //     .add(newUser) //forcing shape of new user >> .add(newUser)
+  //     .then((doc) => {
+  //       //TODO:create a jwt & send back token
+  res.status(201).send("Account created!");
+  // })
+  // .catch((err) => res.status(500).send(err));
 };
