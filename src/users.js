2af8d319f22fc5ffdd7670d20f3cb47f6b9cf19b
Diana Marcelin <marcelindiana@yahoo.com>
2022-02-28 11:51:20 -0500

    connected to fb

--- src/users.js
+++ src/users.js
@@ -1,6 +1,8 @@
+const { connectDb } = require("./dbConnect");
+
 exports.createUser = (req, res) => {
   //first, let's do validation...(email, password)
-  if (!req.body || !req.body.email || req.body.password) {
+  if (!req.body || !req.body.email || !req.body.password) {
     //invalid request
     res.status(400).send("Invalid Request");
     return;
@@ -11,12 +13,12 @@ exports.createUser = (req, res) => {
     isAdmin: false,
     userRole: 5,
   };
-  //   const db = connectDb();
-  //   db.collection("users")
-  //     .add(newUser) //forcing shape of new user >> .add(newUser)
-  //     .then((doc) => {
-  //       //TODO:create a jwt & send back token
-  res.status(201).send("Account created!");
-  // })
-  // .catch((err) => res.status(500).send(err));
+  const db = connectDb();
+  db.collection("users")
+    .add(newUser) //forcing shape of new user >> .add(newUser)
+    .then((doc) => {
+      ////TODO:create a jwt & send back token
+      res.status(201).send("Account created!");
+    })
+    .catch((err) => res.status(500).send(err));
 };
