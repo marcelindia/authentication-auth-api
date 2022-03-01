17ebe231713fb707c12877a28464b0eaa2d60637
Diana Marcelin <marcelindiana@yahoo.com>
2022-03-01 09:32:23 -0500

    req for user login

--- src/users.js
+++ src/users.js
@@ -8,7 +8,7 @@ exports.createUser = (req, res) => {
     return;
   }
   const newUser = {
-    email: req.body.email,
+    email: req.body.email.toLowerCase(),
     password: req.body.password,
     isAdmin: false,
     userRole: 5,
@@ -17,8 +17,66 @@ exports.createUser = (req, res) => {
   db.collection("users")
     .add(newUser) //forcing shape of new user >> .add(newUser)
     .then((doc) => {
+      const user = {
+        // this will become payload for our jwt
+        id: doc.id,
+        email: newUser.email,
+        isAdmin: false,
+        userRole: 5,
+      };
       ////TODO:create a jwt & send back token
-      res.status(201).send("Account created!");
+      res.status(201).send({
+        success: true,
+        message: "Account created",
+        token: user, //add this to token later
+      });
     })
-    .catch((err) => res.status(500).send(err));
+    .catch((err) =>
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      })
+    );
+};
+exports.loginUser = (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    //invalid request
+    res.status(400).send("Invalid Request");
+    return;
+  }
+  const db = connectDb();
+  db.collection("users")
+    .where("email", "==", req.body.email.toLowerCase())
+    .where("password", "==", req.body.password)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        //bad login
+        res.status(401).send({
+          success: false,
+          message: "Invalid email or password",
+        });
+        return;
+      }
+      //good login
+      const users = snapshot.docs.map((doc) => {
+        let user = doc.data();
+        user.id = doc.id;
+        user.password = undefined;
+        return user;
+      });
+      res.send({
+        success: true,
+        message: "Login Successful",
+        token: users[0],
+      });
+    })
+    .catch((err) =>
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      })
+    );
 };
